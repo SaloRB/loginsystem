06d2711f6d558e41aff7f249357c0692ff4178b6
Salomon Romano <salo_romano@yahoo.com>
2020-01-05 14:24:14 -0600

    Added favicon.ico

--- header.php
+++ header.php
@@ -28,7 +28,7 @@ session_start();
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse">
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item">
                         <a class="nav-link" href="index.php">Home</a>
